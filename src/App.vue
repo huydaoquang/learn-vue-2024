c1189bc7249a7dc3fcaad7486a57c2f6709bab8d
huy.daoquang <huycd02@gmail.com>
2024-08-16 09:08:00 +0700

    event handling

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
 	<div class="flex justify-center items-center flex-col h-screen">
-		<p>{{ hello("huydaoquang") }}</p>
-		<p>{{ xinChao("huy") }}</p>
+		<p>{{ name }} -- {{ age }}</p>
 
-		<button
-			@click="tangSo"
-			type="button"
-			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-		>
-			tang
-		</button>
-		<button
-			@click="giamSo"
-			type="button"
-			:disabled="isDisabled"
-			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-		>
-			giam
-		</button>
-
-		<span>{{ count }}</span>
+		<form action="" @submit.prevent="formSubmit">
+			<button
+				class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+			>
+				Submit
+			</button>
+			<input
+				v-model="name"
+				id="email"
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="name@flowbite.com"
+				required
+			/>
+			<input
+				v-model="age"
+				id="email"
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="name@flowbite.com"
+				required
+			/>
+		</form>
 	</div>
 </template>
 
@@ -27,27 +30,18 @@
 export default {
 	data() {
 		return {
-			full_name: "daoquanghuy",
-			count: 0,
-			isDisabled: false,
+			name: "huy",
+			age: 30,
 		};
 	},
 	methods: {
-		hello(fullName) {
-			return "xin chao " + fullName;
+		onChangeName(event) {
+			// console.log("event: ", event);
+			this.name = event.target.value;
 		},
-		xinChao: (name) => {
-			return "xin chao " + name;
-		},
-		tangSo() {
-			this.count++;
-			this.isDisabled = this.count <= 0 ? true : false;
-		},
-		giamSo() {
-			if (this.count > 0) {
-				this.isDisabled = this.count <= 0 ? true : false;
-				this.count--;
-			}
+		formSubmit() {
+			alert(this.name);
+			alert(this.age);
 		},
 	},
 };
