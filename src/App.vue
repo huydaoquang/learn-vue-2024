75d7b5c2ae4fd040e8c91964058c4835119d62fc
huy.daoquang <huycd02@gmail.com>
2024-08-16 14:01:57 +0700

    Input Binding (Two-way databinding)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,9 @@
 <template>
-	<div class="flex justify-center items-center flex-col h-screen">
-		<p>{{ name }} -- {{ age }}</p>
-
-		<form action="" @submit.prevent="formSubmit">
-			<button
-				class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-			>
-				Submit
-			</button>
-			<input
-				v-model="name"
-				id="email"
-				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-				placeholder="name@flowbite.com"
-				required
-			/>
-			<input
-				v-model="age"
-				id="email"
-				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-				placeholder="name@flowbite.com"
-				required
-			/>
+	<div class="flex justify-center items-center h-screen">
+		<form action="" @submit.prevent="formSubmit" class="flex flex-col">
+			name: <input type="text" v-model="formData.name" /> email:
+			<input type="text" v-model="formData.email" />
+			<button>submit</button>
 		</form>
 	</div>
 </template>
@@ -30,18 +12,15 @@
 export default {
 	data() {
 		return {
-			name: "huy",
-			age: 30,
+			formData: {
+				name: "",
+				email: "",
+			},
 		};
 	},
 	methods: {
-		onChangeName(event) {
-			// console.log("event: ", event);
-			this.name = event.target.value;
-		},
 		formSubmit() {
-			alert(this.name);
-			alert(this.age);
+			console.log(this.formData);
 		},
 	},
 };
